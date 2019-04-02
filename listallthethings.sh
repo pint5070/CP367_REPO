675b9fa0ed1fc8d813e5c6ed91a484c98531f5dd
Jake <gr8t1999@gmail.com>
2019-04-02 14:10:00 -0400

    second version of question 1

--- listallthethings.sh
+++ listallthethings.sh
@@ -1,14 +1,22 @@
-if [ $# -eq 1 ]
+if [ $# -gt 0 ]
 then
-    echo proceeding
-    
-    dirs=(*/)
+    for args in $@
+    do
+	
+    echo "proceeding $args"
 
+    count=$(ls -l | grep -c ^d)
+    echo "count: $count"
+    if [ "$count" -ne 0 ]
+       then
+       dirs=(*/)
+    
     for d in "${dirs[@]}"
     do
 	cd "$d"
-	pwd
-	val=$(find -name "$1")
+	
+	val=$(find -name $args)
+	echo $val
 	if [ -z "$val" ]
 	then
 	    echo not found
@@ -16,22 +24,20 @@ then
 	    
 	else
 	    ls -l $val
+	    
 	    echo found
 	    cd ..
-	    
+	    break 1
 	    
 	fi
 	
     done
-    
-    
-
-   
-    
-
-    
-    
-
+    else
+	ls -l | grep 
+    fi    
+        
+        
+done
 else
     echo no argument given
 
